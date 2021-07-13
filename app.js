5da6689961217b9857ef7024aacd84e810cf714f
odai-sadaqa <sadaqa.odai@yahoo.com>
2021-07-13 19:04:21 +0300

    finished lab06

--- app.js
+++ app.js
@@ -12,26 +12,22 @@ let Seattle = {
     maxCustomer: 65,
     minCustomer: 23,
     averCoockies: 6.3,
-    customerPerhours: [],
-    avgCookiesperHour: [],
+    customersEachHour: [],
+    CookiesEachHour: [],
     total: 0,
 
-    // this.customerPerhours=random(23,65)+'cookeies'
-    // console.log(this.customerPerhours);
-    // console.log(this);
 
-
-    coustomer: function () {
+    calcCustomersEachHour: function () {
         for (let i = 0; i < hours.length; i++) {
-            this.customerPerhours.push(random(this.minCustomer, this.maxCustomer));
+            this.customersEachHour.push(random(this.minCustomer, this.maxCustomer));
         }
 
+
     },
-    calAvgCookiesperHour: function () {
+    calcCookiesEachHour: function () {
         for (let i = 0; i < hours.length; i++) {
-            this.avgCookiesperHour.push(Math.floor(this.customerPerhours[i] * this.averCoockies));
-            
-            this.total += this.averCoockies[i];
+            this.CookiesEachHour.push(Math.floor(this.customersEachHour[i] * this.averCoockies));
+            this.total += this.CookiesEachHour[i];
         }
     },
     render: function () {
@@ -50,86 +46,265 @@ let Seattle = {
             let liElement = document.createElement('li');
             unorderdlist.appendChild(liElement);
 
-            liElement.textContent = `${hours[i]}: ${this.coustomer[i]} cookies`;
+            liElement.textContent = `${hours[i]}: ${this.CookiesEachHour[i]} cookies`;
 
         }
 
-
         let totalElement = document.createElement('li');
         unorderdlist.appendChild(totalElement);
-        totalElement.textContent= `Total : ${this.total} cookies`;
+        totalElement.textContent = `Total : ${this.total} cookies`;
 
 
     }
 
 }
 
-Seattle.coustomer();
-Seattle.calAvgCookiesperHour();
+
+Seattle.calcCustomersEachHour();
+Seattle.calcCookiesEachHour();
 Seattle.render();
+
 
 let Tokyo = {
     name: 'Tokyo',
     maxCustomer: 24,
-    minCusomer: 3,
+    minCustomer: 3,
     averCoockies: 1.2,
-    customerPerhours: 0,
-    randomCustomer: [],
-    avgCookiesperHour: [],
-    getcustomerPerhours: function () {
-        this.customerPerhours = random(3, 24) + 'cookeies'
-        console.log(this.customerPerhours);
-        console.log(this);
+    customersEachHour: [],
+    CookiesEachHour: [],
+    total: 0,
+
+
+    calcCustomersEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.customersEachHour.push(random(this.minCustomer, this.maxCustomer));
+        }
+
+
+    },
+    calcCookiesEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.CookiesEachHour.push(Math.floor(this.customersEachHour[i] * this.averCoockies));
+            this.total += this.CookiesEachHour[i];
+        }
+    },
+    render: function () {
+        let parent = document.getElementById('parent');
+        console.log(parent);
+
+        let h2elememnt = document.createElement('h2');
+        console.log(h2elememnt);
+        parent.appendChild(h2elememnt);
+        h2elememnt.textContent = this.name;
+
+        let unorderdlist = document.createElement('ul');
+        parent.appendChild(unorderdlist);
+        // because we need 14 li and this hours array have 14 item
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            unorderdlist.appendChild(liElement);
+
+            liElement.textContent = `${hours[i]}: ${this.CookiesEachHour[i]} cookies`;
+
+        }
+
+        let totalElement = document.createElement('li');
+        unorderdlist.appendChild(totalElement);
+        totalElement.textContent = `Total : ${this.total} cookies`;
+
+
     }
+
 }
-Tokyo.getcustomerPerhours();
+
+Tokyo.calcCustomersEachHour();
+Tokyo.calcCookiesEachHour();
+Tokyo.render();
 
 
 let Dubai = {
     name: 'Dubai',
     maxCustomer: 38,
-    minCusomer: 11,
+    minCustomer: 11,
     averCoockies: 1.2,
-    customerPerhours: 0,
-    randomCustomer: [],
-    avgCookiesperHour: [],
-    getcustomerPerhours: function () {
-        this.customerPerhours = random(11, 38) + 'cookeies'
-        console.log(this.customerPerhours);
-        console.log(this);
+    customersEachHour: [],
+    CookiesEachHour: [],
+    total: 0,
+
+
+    calcCustomersEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.customersEachHour.push(random(this.minCustomer, this.maxCustomer));
+        }
+
+
+    },
+    calcCookiesEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.CookiesEachHour.push(Math.floor(this.customersEachHour[i] * this.averCoockies));
+            this.total += this.CookiesEachHour[i];
+        }
+    },
+    render: function () {
+        let parent = document.getElementById('parent');
+        console.log(parent);
+
+        let h2elememnt = document.createElement('h2');
+        console.log(h2elememnt);
+        parent.appendChild(h2elememnt);
+        h2elememnt.textContent = this.name;
+
+        let unorderdlist = document.createElement('ul');
+        parent.appendChild(unorderdlist);
+        // because we need 14 li and this hours array have 14 item
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            unorderdlist.appendChild(liElement);
+
+            liElement.textContent = `${hours[i]}: ${this.CookiesEachHour[i]} cookies`;
+
+        }
+
+        let totalElement = document.createElement('li');
+        unorderdlist.appendChild(totalElement);
+        totalElement.textContent = `Total : ${this.total} cookies`;
+
+
     }
+
 }
-Dubai.getcustomerPerhours();
+
+Dubai.calcCustomersEachHour();
+Dubai.calcCookiesEachHour();
+Dubai.render();
+
+
+
+
+
+
 
 
 let Paris = {
     name: 'Paris',
-    maxCustomer: 38,
-    minCusomer: 20,
+   maxCustomer: 38,
+    minCustomer: 11,
     averCoockies: 1.2,
-    customerPerhours: 0,
-    randomCustomer: [],
-    avgCookiesperHour: [],
-    getcustomerPerhours: function () {
-        this.customerPerhours = random(20, 38) + 'cookeies'
-        console.log(this.customerPerhours);
-        console.log(this);
+    customersEachHour: [],
+    CookiesEachHour: [],
+    total: 0,
+
+
+    calcCustomersEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.customersEachHour.push(random(this.minCustomer, this.maxCustomer));
+        }
+
+
+    },
+    calcCookiesEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.CookiesEachHour.push(Math.floor(this.customersEachHour[i] * this.averCoockies));
+            this.total += this.CookiesEachHour[i];
+        }
+    },
+    render: function () {
+        let parent = document.getElementById('parent');
+        console.log(parent);
+
+        let h2elememnt = document.createElement('h2');
+        console.log(h2elememnt);
+        parent.appendChild(h2elememnt);
+        h2elememnt.textContent = this.name;
+
+        let unorderdlist = document.createElement('ul');
+        parent.appendChild(unorderdlist);
+        // because we need 14 li and this hours array have 14 item
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            unorderdlist.appendChild(liElement);
+
+            liElement.textContent = `${hours[i]}: ${this.CookiesEachHour[i]} cookies`;
+
+        }
+
+        let totalElement = document.createElement('li');
+        unorderdlist.appendChild(totalElement);
+        totalElement.textContent = `Total : ${this.total} cookies`;
+
+
     }
+
+
 }
-Paris.getcustomerPerhours();
+Paris.calcCustomersEachHour();
+Paris.calcCookiesEachHour();
+Paris.render();
+
+
+
+
 
 let Lima = {
     name: 'Lima',
-    maxCustomer: 16,
-    minCusomer: 2,
-    averCoockies: 1.2,
-    customerPerhours: 0,
-    randomCustomer: [],
-    avgCookiesperHour: [],
-    getcustomerPerhours: function () {
-        this.customerPerhours = random(2, 16) + 'cookeies'
-        console.log(this.customerPerhours);
-        console.log(this);
+   maxCustomer: 2,
+    minCustomer: 16,
+    averCoockies: 4.6,
+    customersEachHour: [],
+    CookiesEachHour: [],
+    total: 0,
+
+
+    calcCustomersEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.customersEachHour.push(random(this.minCustomer, this.maxCustomer));
+        }
+
+
+    },
+    calcCookiesEachHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+            this.CookiesEachHour.push(Math.floor(this.customersEachHour[i] * this.averCoockies));
+            this.total += this.CookiesEachHour[i];
+        }
+    },
+    render: function () {
+        let parent = document.getElementById('parent');
+        console.log(parent);
+
+        let h2elememnt = document.createElement('h2');
+        console.log(h2elememnt);
+        parent.appendChild(h2elememnt);
+        h2elememnt.textContent = this.name;
+
+        let unorderdlist = document.createElement('ul');
+        parent.appendChild(unorderdlist);
+        // because we need 14 li and this hours array have 14 item
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            unorderdlist.appendChild(liElement);
+
+            liElement.textContent = `${hours[i]}: ${this.CookiesEachHour[i]} cookies`;
+
+        }
+
+        let totalElement = document.createElement('li');
+        unorderdlist.appendChild(totalElement);
+        totalElement.textContent = `Total : ${this.total} cookies`;
+
+
     }
+
+
+
 }
-Lima.getcustomerPerhours();
+
+Lima.calcCustomersEachHour();
+Lima.calcCookiesEachHour();
+Lima.render();
+
+
+
+
+
+
